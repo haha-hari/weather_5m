641939ae53f8eaad7da26527da998f97096cfd8e
dla0257-sudo <dla0257@gmail.com>
2026-03-05 14:45:26 +0900

    Update weather.py for API key import and CSV writing
    Refactor to import os directly and fix newline handling in CSV.

--- weather.py
+++ weather.py
@@ -2,8 +2,7 @@
 import requests # get방식으로 요청할 때 사용하는 라이브러리
 import csv
 from datetime import datetime
-import os.getenv("WEATHER_API_KEY")
-# KEY는 나중에 암호화!!
+import os
 MY_API_KEY = os.getenv("WEATHER_API_KEY")
 
 city_name = "Seoul"
@@ -19,10 +18,9 @@ current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # weather.csv를 만들자
 # 최초 생성 시 -> 헤더도 추가 파일이 존재하면 -> 덮어쓰기
-
 csv_exist = os.path.exists("weather.csv")
 header = ["current_time", "temp", "humidity", "main"]
-with open("weather.csv", "a") as f:
+with open("weather.csv", "a", newline="") as f:
     writer = csv.writer(f)
     if not csv_exist:
         writer.writerow(header)
